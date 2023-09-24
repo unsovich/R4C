f8d8c16c2edd06bf3c9bb520879f71b42a07855d
Anton Unsovich <unsovich@gmail.com>
2023-09-24 16:21:10 -0300

    Task 2 v.1 unnecessary class ProductionReport was deleted

--- robots/models.py
+++ robots/models.py
@@ -14,23 +14,3 @@ class Robot(models.Model):
 def generate_serial(sender, instance, **kwargs):
     if not instance.serial:  # Генерировать serial если пустой
         instance.serial = f"{instance.model}-{instance.version}"
-
-
-class ProductionReport(models.Model):
-    start_date = models.DateField()
-    end_date = models.DateField()
-    robot_model = models.CharField(max_length=2)
-    robot_version = models.CharField(max_length=2)
-    total_production = models.PositiveIntegerField()
-
-    @staticmethod
-    def generate_report(start_date, end_date, robot_model, robot_version):
-        total_production = Robot.objects.filter(
-            model=robot_model, version=robot_version,
-            created__gte=start_date, created__lte=end_date
-        ).count()
-        return total_production
-
-    def __str__(self):
-        return (f"Отчет о производстве {self.robot_model} ({self.robot_version}) "
-                f"за период с {self.start_date} по {self.end_date}")
