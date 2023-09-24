b7237ddbeeaa8f3bbf4dffbb2c925aefbef0b428
Anton Unsovich <unsovich@gmail.com>
2023-09-23 21:24:40 -0300

    Task 1 v.1 completed

--- robots/apps.py
+++ robots/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 
 class RobotsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'  # для поддержки больших автоинкрементных полей
     name = 'robots'
